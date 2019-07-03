08ceae0e7c9a14d8f7de93e7bd0f0c283ad2d53a
Alan <dima30051999@mail.ru>
2019-07-03 16:06:49 +0700

    first commit

---
+++ src/main/java/ru/cft/starterkit/api/SampleEntityController.java
@@ -0,0 +1,52 @@
+package ru.cft.starterkit.api;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+import ru.cft.starterkit.entity.SampleEntity;
+import ru.cft.starterkit.exception.ObjectNotFoundException;
+import ru.cft.starterkit.service.SampleEntityService;
+
+import java.util.Collection;
+
+@RestController(Paths.SAMPLES)
+public class SampleEntityController {
+
+    private final SampleEntityService sampleEntityService;
+
+    @Autowired
+    public SampleEntityController(SampleEntityService sampleEntityService) {
+        this.sampleEntityService = sampleEntityService;
+    }
+
+    @PostMapping(
+            consumes = "application/x-www-form-urlencoded",
+            produces = "application/json"
+    )
+    public SampleEntity add(
+            @RequestParam(name = "foo") String foo,
+            @RequestParam(name = "bar", defaultValue = "1.1") Double bar
+    ) {
+        return sampleEntityService.add(foo, bar);
+    }
+
+    @GetMapping(path = Paths._ID, produces = "application/json")
+    public SampleEntity get(@PathVariable(name = "id") Long id) {
+        try {
+            return sampleEntityService.get(id);
+        } catch (ObjectNotFoundException e) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, e.getMessage(), e);
+        }
+    }
+
+    @GetMapping(path = "/orders", produces = "application/json")
+    public Collection<SampleEntity> getOrders() {
+        return sampleEntityService.getOrders();
+    }
+
+}
---
+++ src/main/java/ru/cft/starterkit/repository/SampleEntityRepository.java
@@ -0,0 +1,16 @@
+package ru.cft.starterkit.repository;
+
+import ru.cft.starterkit.entity.SampleEntity;
+import ru.cft.starterkit.exception.ObjectNotFoundException;
+
+import java.util.Collection;
+
+public interface SampleEntityRepository {
+
+    SampleEntity add(SampleEntity entity);
+
+    SampleEntity get(Long id) throws ObjectNotFoundException;
+
+    Collection<SampleEntity> getOrders();
+
+}
